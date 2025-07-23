1c79632b613805e8148c1dae7bd9b2c8f31b5134
10555gff <1073245193@qq.com>
2025-07-23 13:45:01 -0700

    Add new Impl

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,24 @@ use ternary_utils::logical_calculate::Digit;
 
 fn main() {
 
-    //let t1 = Ternary::parse("++00--");
-    
-    //println!("{:?}",t2);
+    let t1 =Ternary::parse("+++++++0");
+    let t2 =Digit::P;
+
+    let t3=&t1-t2;
+
+    // t1.digits_print();
+    // t2.digits_print();
+    t3.digits_print();
+
+
+
+
+    // let t3=&t1+t2;
+
+    // t1.digits_print();
+    // // t2.digits_print();
+
+    // t3.digits_print();
 
     //let t2=t1.to_neg();
     //let c=t1.sign();
@@ -24,15 +39,15 @@ fn main() {
     //println!("{:?}",c.to_string());
     //c.digits_print();
 
-    let repr9 = Ternary::parse("+00");
-    let repr4 = Ternary::parse("++");
-    let repr13 = &repr9 + &repr4;
-    let repr17 = &repr13 + &repr4;
-    let repr34 = &repr17 + &repr17;
+    // let repr9 = Ternary::parse("+00");
+    // let repr4 = Ternary::parse("++");
+    // let repr13 = &repr9 + &repr4;
+    // let repr17 = &repr13 + &repr4;
+    // let repr34 = &repr17 + &repr17;
 
-    repr13.digits_print();
-    repr17.digits_print();
-    repr34.digits_print();
+    // repr13.digits_print();
+    // repr17.digits_print();
+    // repr34.digits_print();
 
     // let a = Ternary::from_dec(5);
     // let d = Digit::N; // -1
@@ -54,12 +69,23 @@ fn main() {
 
     // println!("result :{:?}",a.half_adder(b));
 
-    let a = Digit::P;
-    let b = Digit::N;
-    let result = a - b;
-    // println!("sum = {:?}, carry = {:?}", result.sum, result.carry);
-    println!("{:?}",result);
+    // let a = Digit::P;
+    // let b = Digit::N;
+    // let result = a - b;
+    // // println!("sum = {:?}, carry = {:?}", result.sum, result.carry);
+    // println!("{:?}",result);
 
-
+    // let test = Ternary::from_dec(18887455);
+    // test.digits_print_t();
+    // assert_eq!(test.to_string(), "++00--0--+-0++0+");
     
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world!");
+    // let s3 = s1 + &s2;//会消耗左值
+
+    // println!("{}",s2);
+    // println!("{}",s3);
+    // //println!("{}",s1);
+//     let c1 = &a + &b; // 不消耗 a 和 b
+// let c2 = a + b;   // 消耗所有权
 }
--- src/ternary_utils/ternary_io.rs
+++ src/ternary_utils/ternary_io.rs
@@ -1,6 +1,5 @@
 use super::logical_calculate::Digit;
-
-use core::ops::{Deref,Add, BitAnd, BitOr, BitXor, Div, Mul, Neg, Not, Sub, Shl, Shr};
+use core::ops::{Deref, DerefMut, Neg, Add, BitAnd, BitOr, BitXor, Div, Mul, Sub, Shl, Shr};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Ternary(pub Vec<Digit>);
@@ -9,6 +8,12 @@ impl Deref for Ternary {
     type Target = Vec<Digit>;
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+impl DerefMut for Ternary {
+    //不需要再次声明type Target ，因为它已经从 Deref 那里继承了
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
@@ -25,16 +30,16 @@ impl Ternary {
         Self(self.iter().copied().map(Digit::to_neg).collect())
     }
 
+    pub fn to_string(&self) -> String {
+        self.iter().map(Digit::to_char).collect()
+    }
+
     pub fn to_sign(&self) -> Digit {
         let mut state=Digit::Z;
         for &digit in self.iter() {
             state=state.tpoz(digit);
         }
         state
-    }
-
-    pub fn to_string(&self) -> String {
-        self.iter().map(Digit::to_char).collect()
     }
 
     /// Converts the `Ternary` object to its integer (decimal) representation.
@@ -96,10 +101,8 @@ impl Ternary {
 
     pub fn adder_base(&self, other: &Self, mut c_in: Digit)-> Self{
         let mut result:Vec<Digit> = Vec::new();//存储和
-
         let max_len=self.len().max(other.len());
-        let stack1=self.pad_left(max_len);
-        let stack2=other.pad_left(max_len);
+        let (stack1,stack2)=(self.pad_left(max_len),other.pad_left(max_len));
 
         for i in (0..max_len).rev(){
             let (v1,v2)=(stack1[i],stack2[i]);
@@ -115,39 +118,93 @@ impl Ternary {
         Self(result)
     }
 
+
+    pub fn adder_stack(mut self, mut other: Self, mut c_in: Digit) -> Self {
+        let mut result:Vec<Digit> = Vec::new();//存储和
+        
+        //Rust标准库中Vec,天然支持后进先出（LIFO），用栈协同弹出，倒序遍历, 支持不同长度
+        while !self.is_empty() || !other.is_empty() {
+            let v1 = self.pop().unwrap_or(Digit::Z);
+            let v2 = other.pop().unwrap_or(Digit::Z);
+    
+            let fulladder_out=v1.full_adder(v2, c_in);
+            result.push(fulladder_out.sum);//存结果
+            c_in=fulladder_out.carry;//进位传递
+        }
+        if c_in!=Digit::Z{
+            result.push(c_in);
+        }
+
+        result.reverse(); // 反转，从高位到低位排列
+        Self(result)
+    }
+
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-impl Add<&Ternary> for &Ternary {
+impl Neg for &Ternary {//不消耗原值，借用
     type Output = Ternary;
+
+    fn neg(self) -> Self::Output {
+        self.to_neg()
+    }
+}
+impl Neg for Ternary {//消耗原值，借用自身
+    type Output = Ternary;
+
+    fn neg(self) -> Self::Output {
+        (&self).to_neg()
+    }
+}
+
+impl Add<&Ternary> for &Ternary {//&a + &b，不消耗原值，适合重用,借用 和 借用
+    type Output = Ternary;
+
     fn add(self, rhs: &Ternary) -> Self::Output {
         self.adder_base(rhs, Digit::Z)
     }
 }
+impl Add<Ternary> for Ternary {//a + b，消耗原值，优化内存操作,所有权 和 所有权
+    type Output = Ternary;
 
-impl Add<Digit> for &Ternary {
+    fn add(self, rhs: Ternary) -> Self::Output {
+        self.adder_stack(rhs, Digit::Z)
+    }
+}
+impl Add<Digit> for &Ternary {//&a + b，Ternary+Digit
     type Output = Ternary;
 
     fn add(self, rhs: Digit) -> Self::Output {
-        Ternary::from_dec(
-            self.to_dec()
-                .checked_add(rhs.to_i8() as i64)
-                .expect("Overflow in addition."),
-        )
+        let other=Ternary::new(vec![0]);
+        self.adder_base(&other, rhs)
+    }
+}
+
+
+
+impl Sub<&Ternary> for &Ternary {//&a - &b，不消耗原值，适合重用,借用 和 借用
+    type Output = Ternary;
+
+    fn sub(self, rhs: &Ternary) -> Self::Output {
+        let other=-rhs;//获取相反数
+        self.adder_base(&other, Digit::Z)
+    }
+}
+impl Sub<Ternary> for Ternary {//a - b，消耗原值，优化内存操作,所有权 和 所有权
+    type Output = Ternary;
+
+    fn sub(self, rhs: Ternary) -> Self::Output {
+        let other=-rhs;//获取相反数
+        self.adder_stack(other, Digit::Z)
+    }
+}
+impl Sub<Digit> for &Ternary {//&a - b，Ternary-Digit
+    type Output = Ternary;
+
+    fn sub(self, rhs: Digit) -> Self::Output {
+        let other=Ternary::new(vec![0]);
+        self.adder_base(&other, rhs.to_neg())
     }
 }
 
