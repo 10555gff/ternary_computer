69ab562179e62a3fbba2b4550b700454d0fc83db
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2026-02-13 22:53:13 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+ use ternary_arithmetic::ternary_cpu::trit::Trit4;
 
 fn main() {
-    println!("Hello World!");
+    let reg_a = Trit4(0b01_00_10_01); // 对应三进制序列 [P, Z, N, P]
+    let reg_b = Trit4(0b00_01_01_10); // 对应三进制序列 [Z, P, P, N]
+
+    // 执行并行或运算
+    let result = reg_a.dibit_tor(reg_b);
+    
+
+    println!("结果内部值: {:08b}", result);
 }
