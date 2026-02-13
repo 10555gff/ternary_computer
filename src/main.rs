d1ee0ffb0df797d4a53d63b653aa7916aeacc0c4
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2026-02-14 05:18:27 +0800

    aka

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,20 @@ use ternary_arithmetic::ternary_cpu::trit::Trit4;
 
 
 fn main() {
-    let a = Trit4(0b10_00_01_00); 
-    let b = Trit4(0b10_10_10_00);
-    let c = Trit4(0b00_00_00_00);
-    let d = Trit4(0b01_01_01_00);
+    //let a = Trit4(0b10_00_01_00); 
+    // let b = Trit4(0b10_10_10_00);
+    // let c = Trit4(0b00_00_00_00);
+    // let d = Trit4(0b01_01_01_00);
+
+    let a = Trit4(0b01_01_10_01); 
     
-    let result1 = a.tor(b);
-    let result2 = a.tor(c);
-    let result3 = a.tor(d);
+    let result1 = a.tneg();
+    // let result2 = a.tor(c);
+    // let result3 = a.tor(d);
 
 
 
     println!("结果  : {:08b}", result1.0);
-    println!("结果  : {:08b}", result2.0);
-    println!("结果  : {:08b}", result3.0);
+    // println!("结果  : {:08b}", result2.0);
+    // println!("结果  : {:08b}", result3.0);
 }
