1cf296aff7d9784672bd10739cdd2117a6cd41a6
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2026-02-17 16:38:01 +0800

    target

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
-use ternary_arithmetic::ternary_cpu::trit::Trit4;
-use ternary_arithmetic::ternary_cpu::logical_table::Trit;
+const SHIFT: [u8;4] = [0,2,4,6];
+const MASK:  [u8;4] = [0x03,0x0C,0x30,0xC0];
+
+fn set_2bit(word: u8, k: usize, value: u8) -> u8 {
+    let mask  = MASK[k];
+    let shift = SHIFT[k];
+    (word & !mask) | ((value & 0x03) << shift)
+}
+fn read_2bit(word:u8,k:usize)->u8{
+    (word & MASK[k]) >> SHIFT[k]
+}
+fn clear_2bit(word: u8, k: usize) -> u8 {
+    word & !MASK[k]
+}
+fn toggle_2bit(word: u8, k: usize) -> u8 {
+    word ^ MASK[k]
+}
+
+
+fn set2(word: u8, k: u8, value: u8) -> u8 {
+    let shift = k * 2;
+    let mask = 0x03 << shift;
+    // 先将目标位置清零，再存入新值
+    (word & !mask) | ((value & 0x03) << shift)
+}
+
+fn read2(word: u8, k: u8) -> u8 {
+    (word >> (k * 2)) & 0x03
+}
+
+fn clear2(word: u8, k: u8) -> u8 {
+    let mask = 0x03 << (k * 2);
+    word & !mask
+}
+
+fn toggle2(word: u8, k: u8) -> u8 {
+    let mask = 0x03 << (k * 2);
+    word ^ mask
+}
+
+
+// fn set(word:u8,n:u8)->u8{
+//     return word | (1 << n)
+// }
+// fn clear(word:u8,n:u8)->u8{
+//     return word & !(1 << n)
+// }
+// fn toggle(word:u8,n:u8)->u8{
+//     return word ^ (1 << n)
+// }
+// fn read(word:u8,n:u8)->u8{
+//     return (word >> n) & 1
+// }
+
+
+
 fn main() {
-    // let a = Trit4(0b10_00_01_00); 
-    // let b = Trit4(0b10_10_10_00);
-    // let c = Trit4(0b00_00_00_00);
-    // let d = Trit4(0b01_01_01_00);
-    // let code=3;
-    
-    // let result1 =a.gate_core(b,code);
-    // let result2 = a.gate_core(c,code);
-    // let result3 = a.gate_core(d,code);
-
-
-
-    // println!("结果  : {:08b}", result1.0);
-    // println!("结果  : {:08b}", result2.0);
-    // println!("结果  : {:08b}", result3.0);
-
-    let a = Trit4(0b00_01_01_01); 
-    let b = Trit4(0b00_10_00_00);
-    let res=a.dibit_adder(b,Trit::P);
-    println!("结果:{:?}{:08b}",res.0,res.1.0);
-
-
-
+    let word:u8=0b11_10_11_01;
+    let a=toggle2(word,3);
+    println!("result:{:08b}",a);
 }
