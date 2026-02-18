aa0130d36e323a33a68d85669632f2789874cdf6
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2026-02-18 15:27:07 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
-use ternary_arithmetic::ternary_cpu::trit::Trit4;
+// use ternary_arithmetic::ternary_cpu::trit::Trit4;
+
+use trit_macro::trits;
+
+
 
 fn main() {
-    let a = Trit4(0b10_00_01_00); 
-    let b = Trit4(0b10_10_10_00);
-    let c = Trit4(0b00_00_00_00);
-    let d = Trit4(0b01_01_01_00);
-    let code=7;
+    // 现在的体验就像原生语法一样！
+    let a = trits!("+-0+"); 
+    // let b = trit!(TOI);
     
-    let result1 =a.gate_core(b,code);
-    let result2 =a.gate_core(c,code);
-    let result3 =a.gate_core(d,code);
-
-
-
-    // let result1 = a.tncons(b);
-    // let result2 = a.tncons(c);
-    // let result3 = a.tncons(d);
-    println!("结果  : {:08b}", result1.0);
-    println!("结果  : {:08b}", result2.0);
-    println!("结果  : {:08b}", result3.0);
+    println!("a: {:08b}", a); // 10101010
+    // println!("b: {:08b}", b.0); // 100001
 }
+
+
+
+// fn main() {
+//     let a = Trit4(0b10_00_01_00); 
+//     let b = Trit4(0b10_10_10_00);
+//     let c = Trit4(0b00_00_00_00);
+//     let d = Trit4(0b01_01_01_00);
+//     let code=7;
+    
+//     let result1 =a.gate_core(b,code);
+//     let result2 =a.gate_core(c,code);
+//     let result3 =a.gate_core(d,code);
+
+
+
+//     // let result1 = a.tncons(b);
+//     // let result2 = a.tncons(c);
+//     // let result3 = a.tncons(d);
+//     println!("结果  : {:08b}", result1.0);
+//     println!("结果  : {:08b}", result2.0);
+//     println!("结果  : {:08b}", result3.0);
+// }
--- trit_macro/src/lib.rs
+++ trit_macro/src/lib.rs
@@ -3,19 +3,22 @@ use quote::quote;
 use syn::{parse_macro_input, LitStr};
 
 #[proc_macro]
-pub fn trit(input: TokenStream) -> TokenStream {
+pub fn trits(input: TokenStream) -> TokenStream {
     let lit = parse_macro_input!(input as LitStr);
     let s = lit.value();
 
-    let mut value: u32 = 0;
+    let mut value: u8 = 0;
     for c in s.chars() {
+        // 允许用户使用下划线增加可读性，如 trits!(11_TT)
+        if c == '_' { continue; }
+
         value <<= 2;
-        value |= match c {
-            '-' | 'T' => 0b10,
-            '0' => 0b00,
-            '+' | '1' => 0b01,
+        match c {
+            '-' | 'T' => value |= 0b10,
+            '0'       => value |= 0b00,
+            '+' | '1' => value |= 0b01,
             _ => panic!("invalid trit"),
-        };
+        }
     }
 
     let out = quote! { #value };
