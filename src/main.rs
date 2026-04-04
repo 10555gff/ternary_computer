ec5c94edd9397e20751d25c56ef1a8433c024991
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2026-04-04 18:40:32 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,49 @@ fn main() {
     // let d = trits!("+++0_+++0_+++0_+++0");
 
 
-    // let a = trits!("00+0_++0+");
-    // let b = trits!("0000_0+0-");
-    // let re = a.div(b);//
-    // println!("quotient:{},remainder:{}",re.0,re.1);
+    let a = trits!("00+0_++0+");
+    let b = trits!("0000_0+0-");
+    let re = a.div(b);//280/8
+    println!("quotient:{},remainder:{}",re.0,re.1);
 
-    // let a = trits!("0000_++++");
-    // let b = trits!("0000_0+0-");
-    // let re = a.div(b);//40/8
-    // println!("quotient:{},remainder:{}",re.0,re.1);
+    let a = trits!("0000_++++");
+    let b = trits!("0000_0+0-");
+    let re = a.div(b);//40/8
+    println!("quotient:{},remainder:{}",re.0,re.1);
 
-    // let a = trits!("0000_0+0+");
-    // let b = trits!("0000_00+-");
-    // let re = a.div(b);//10/2
-    // println!("quotient:{},remainder:{}",re.0,re.1);
+    let a = trits!("0000_0+0+");
+    let b = trits!("0000_00+-");
+    let re = a.div(b);//10/2
+    println!("quotient:{},remainder:{}",re.0,re.1);
+
+    let a = trits!("0000_0-0-");
+    let b = trits!("0000_00-+");
+    let re = a.div(b);//-10/-2
+    println!("quotient:{},remainder:{}",re.0,re.1);
 
     let a = trits!("0000_0+0+");
     let b = trits!("0000_00-+");
     let re = a.div(b);//10/-2
+    println!("quotient:{},remainder:{}",re.0,re.1);
+
+    let a = trits!("0000_0-0-");
+    let b = trits!("0000_00+-");
+    let re = a.div(b);//-10/2
+    println!("quotient:{},remainder:{}",re.0,re.1);
+
+    let a = trits!("0000_0+0+");
+    let b = trits!("0000_00+0");
+    let re = a.div(b);//10/3
+    println!("quotient:{},remainder:{}",re.0,re.1);
+
+    let a = trits!("000+_-++-");
+    let b = trits!("0000_0+--");
+    let re = a.div(b);//10/3
+    println!("quotient:{},remainder:{}",re.0,re.1);
+
+    let a = trits!("0000_+-++");
+    let b = trits!("0000_0+--");
+    let re = a.div(b);//22/5
     println!("quotient:{},remainder:{}",re.0,re.1);
 
 
